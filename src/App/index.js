dfec1f13605b57a3007e6bcd5258e7fdfecf186c
Irina Marchuk <mar4uk@yandex-team.ru>
2018-09-13 08:35:12 +0300

    Comb App.scss style

--- src/App/index.js
+++ src/App/index.js
@@ -136,11 +136,11 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+                <header className="App__header">
+                    <img src={logo} className="App__logo" alt="logo" />
+                    <h1 className="App__title">Welcome to React</h1>
                 </header>
-                <div className="App-body">
+                <div className="App__body">
                     {this.renderFiler()}
                     {this.renderBreweries()}
                 </div>
