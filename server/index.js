1349190e2f64f1b5c75d22efd35340759bc7f634
“larrotta69” <larrotta69@gmail.com>
2018-10-28 18:45:14 -0400

    depploy

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(path.resolve(__dirname, '../build')));
 
 // views is directory for all template files
@@ -13,6 +11,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
 
-app.listen(app.get('port'), () => {
-    console.log('Node app is running on port', app.get('port'));
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Node app is running on port');
 });
