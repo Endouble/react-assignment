b96fee4704e849fda83c9a61a55ede854ed139eb
“larrotta69” <larrotta69@gmail.com>
2018-10-28 18:31:22 -0400

    deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const fs = require('fs');
-const https = require('https');
 
 const app = express();
 const path = require('path');
@@ -15,10 +13,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
 
-https.createServer({
-    key: fs.readFileSync('./cert/server.key'),
-    cert: fs.readFileSync('./cert/server.cert'),
-}, app)
-    .listen(app.get('port'), () => {
-        console.log('Node app is running on port', app.get('port'));
-    });
+app.listen(app.get('port'), () => {
+    console.log('Node app is running on port', app.get('port'));
+});
