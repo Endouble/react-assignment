3ec41f22a9212bf9b2b2cb80321851933ec9ab94
“larrotta69” <larrotta69@gmail.com>
2018-10-28 14:54:44 -0400

    refactor(pokedex): splitting context and container for better separation of concerns

--- src/modules/PokeDex/PokeDex.container.js
+++ src/modules/PokeDex/PokeDex.container.js
@@ -3,13 +3,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import PokeDex from './PokeDex.component';
+import PokeDexContext from './PokeDex.context';
 import Modal from '../Modal';
 import CardBio from '../Card/CardBio.component';
-
-import { CardsContext, FilterContext } from '../../pages/Home/Home.component';
-
-import getPokemons from '../../api/pokemon';
 
 class PokeDexData extends React.Component {
     constructor(props) {
@@ -18,18 +14,8 @@ class PokeDexData extends React.Component {
             currentCard: {},
             isModalOpen: false,
         };
-        this.filteredCards = this.filteredCards.bind(this);
         this.setCurrentCard = this.setCurrentCard.bind(this);
         this.hideModal = this.hideModal.bind(this);
-    }
-
-    componentDidMount() {
-        getPokemons()
-            .then((data) => {
-                if (data && data.cards) {
-                    this.props.setCards(data.cards);
-                }
-            });
     }
 
     componentDidUpdate(prevProps, prevState) {
@@ -51,24 +37,11 @@ class PokeDexData extends React.Component {
         this.setState({ isModalOpen: false });
     }
 
-    filteredCards(filter, cards) {
-        console.log('filter ----> debug');
-        console.log(filter);
-        const updatedCards = filter.value !== '' ? cards.filter(c => c[filter.filterBy].toLowerCase().includes(filter.value)) : cards;
-        return <PokeDex clickHandler={this.setCurrentCard} cards={updatedCards} />;
-    }
-
     render() {
         const { isModalOpen, currentCard } = this.state;
         return (
             <div>
-                <FilterContext.Consumer>
-                    {filter => (
-                        <CardsContext.Consumer>
-                            {cards => this.filteredCards(filter, cards)}
-                        </CardsContext.Consumer>
-                    )}
-                </FilterContext.Consumer>
+                <PokeDexContext setCards={this.props.setCards} />
                 <Modal isOpen={isModalOpen} hideModal={this.hideModal}>
                     <CardBio {...currentCard} />
                 </Modal>
@@ -82,7 +55,7 @@ PokeDexData.propTypes = {
 };
 
 PokeDexData.defaultProps = {
-    setCards: null,
+    setCards: () => null,
 };
 
 export default PokeDexData;
