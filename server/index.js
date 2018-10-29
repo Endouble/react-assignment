1e38639e031b2c84aaa7c531954d83974f611aa0
“larrotta69” <larrotta69@gmail.com>
2018-10-29 11:50:44 -0400

    deploy

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 
 const app = express();
 const path = require('path');
@@ -9,10 +11,14 @@ app.use(express.static(path.resolve(__dirname, '../build')));
 
 // views is directory for all template files
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log('Node app is running on port');
-});
+https.createServer({
+    key: fs.readFileSync('./cert/server.key'),
+    cert: fs.readFileSync('./cert/server.cert'),
+}, app)
+    .listen(PORT, () => {
+        console.log('Node app is running on port', PORT);
+    });
