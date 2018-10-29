414c47737bd80d91df7c82173b5d4cc84dfdc2d0
“larrotta69” <larrotta69@gmail.com>
2018-10-29 12:54:36 -0400

    deploy

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 
 const app = express();
 const path = require('path');
 
 const PORT = process.env.PORT || 5000;
+const { IS_SECURE } = process.env;
 
 app.use(express.static(path.resolve(__dirname, '../build')));
 
@@ -13,7 +16,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
 
+const runningApp = IS_SECURE ? https.createServer({
+    key: fs.readFileSync('./cert/server.key'),
+    cert: fs.readFileSync('./cert/server.cert'),
+}, app) : app;
 
-app.listen(PORT, () => {
+runningApp.listen(PORT, () => {
     console.log('Node app is running on port', PORT);
 });
