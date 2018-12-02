8976a607852c7ec4628e15f1aef5e585b09405a7
Bruno <brunoalnascimento@gmail.com>
2018-12-02 20:15:14 -0200

    setting up

---
+++ src/utils/test-utils.js
@@ -0,0 +1,10 @@
+// test-utils.js
+// import { render } from 'react-testing-library';
+
+// const customRender = () => render();
+
+// re-export everything
+export * from 'react-testing-library';
+
+// override render method
+// export { customRender as render };
