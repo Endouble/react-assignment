518454ddbc874c4ce6f5168e0b2651190e997dc1
Fabricio Augusto Longhi <flonghi@despegar.com>
2018-12-15 13:54:12 -0300

    state defined and error awareness

--- src/components/CharactersGrid/CharactersGrid.js
+++ src/components/CharactersGrid/CharactersGrid.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const CharactersGrid = () => {
+  
    return <div>
 
   </div>
--- src/containers/CharactersGridContainer/CharactersGridContainer.js
+++ src/containers/CharactersGridContainer/CharactersGridContainer.js
@@ -3,9 +3,24 @@ import React, { Component } from 'react';
 import CharactersGrid from './../../components/CharactersGrid'
 
 class CharactersGridContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      characters: [],
+      isLoading: false,
+      onError: true
+    }
+  }
+
   render() {
+    const { onError, characters, isLoading} = this.state;
     return <React.Fragment>
       <CharactersGrid />
+      {onError?
+        <p> An error ocurred, please try again later... </p>
+        :null
+      }
     </React.Fragment>
   }
 }
