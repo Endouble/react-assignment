d74cc2134ad2810f07ec3648d5de1dd2974a8d3e
Fabricio Augusto Longhi <flonghi@despegar.com>
2018-12-15 17:54:53 -0300

    Modal definition

--- src/components/CharacterModal/CharacterModal.js
+++ src/components/CharacterModal/CharacterModal.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import { Modal } from 'semantic-ui-react';
+
 const CharacterModal = (props) => {
-  return <React.Fragment>
-    
-  </React.Fragment>
+  return <Modal open closeIcon>
+    <Modal.Header>
+      {props.character.name}
+    </Modal.Header>
+    <Modal.Content>
+      <Modal.Description>
+        <p>Gender: {props.character.gender}</p>
+        <p>Eye color: {props.character.eye_color}</p>
+        <p>Height: {props.character.height}</p>
+        <p>Mass: {props.character.mass}</p>
+        <p>Skin color: {props.character.skin_color}</p>
+      </Modal.Description>
+    </Modal.Content>
+  </Modal>
 };
 
 export default CharacterModal;
