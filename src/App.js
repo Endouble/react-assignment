623b7a0aa16700b5e1c9d14bcd943b980a6d8d72
Fabricio Augusto Longhi <flonghi@despegar.com>
2018-12-15 12:56:06 -0300

    cleaned app.css and added semantic-ui default styles to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 
-import { Header, Loader, Message, Icon, Container } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="app-header">
-
+            <Header as='h1' inverted>
+                <Header.Content>
+                    Star Wars Characters
+                    <Header.Subheader>a list from a far far away galaxy</Header.Subheader>
+                </Header.Content>
+            </Header>
         </header>
       </div>
     );
