695e3eccb298e61464f66a55ba557c4f77f2b131
Fabricio Augusto Longhi <flonghi@despegar.com>
2018-12-15 19:16:48 -0300

    define PropTypes for Filter Component

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Button, Form } from 'semantic-ui-react'
 
@@ -20,5 +21,9 @@ const Filters = (props) => {
     <Button type='submit' primary>Search</Button>
   </Form>
 };
+
+Filters.propTypes = {
+  filterCallback: PropTypes.func.isRequired
+}
 
 export default Filters;
