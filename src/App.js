360ca842b41d6115cfe254f0185ceddd8336469e
Fabricio Augusto Longhi <flonghi@despegar.com>
2018-12-15 13:18:17 -0300

    Header factored into AppHeader Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
 
-import { Header } from 'semantic-ui-react';
-
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
+
+import AppHeader from  './components/Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="app-header">
-            <Header as='h1' inverted>
-                <Header.Content>
-                    Star Wars Characters
-                    <Header.Subheader>a list from a far far away galaxy</Header.Subheader>
-                </Header.Content>
-            </Header>
-        </header>
+        <AppHeader />
       </div>
     );
   }
