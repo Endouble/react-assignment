d0daa016807101f679574fc282e9d6b10fb2b347
Fabricio Augusto Longhi <flonghi@despegar.com>
2018-12-16 13:21:37 -0300

    App Component tests

---
+++ src/App.spec.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from './App';
+
+describe('App Test', () => {
+  let shallowApp;
+
+  const getShallowApp = () => {
+    if (!shallowApp) {
+      shallowApp = shallow (<App />)
+    }
+    return shallowApp;
+  };
+
+  beforeEach(() => {
+    shallowApp = undefined;
+  });
+
+  test('Component should render without crashing', () => {
+    expect(getShallowApp()).toBeDefined();
+  });
+
+  test('Component should render correctly', () => {
+    expect(getShallowApp()).toMatchSnapshot();
+  });
+
+  test('Component should render AppHeader', () => {
+    const wrapper = getShallowApp();
+    expect(wrapper.find('AppHeader')).toBeDefined();
+  });
+
+  test('Component should render CharactersGridContainer', () => {
+    const wrapper = getShallowApp();
+    expect(wrapper.find('CharactersGridContainer')).toBeDefined();
+  });
+});
