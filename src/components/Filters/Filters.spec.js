97bf338be323e4eabe12eb2ec9ad463caea46940
Fabricio Augusto Longhi <flonghi@despegar.com>
2018-12-16 12:43:59 -0300

    Filters component tests

---
+++ src/components/Filters/Filters.spec.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import Filters from './Filters';
+
+describe('Filters Test', () => {
+  let shallowFilters;
+
+  const props = {
+    filterCallback: jest.fn()
+  }
+
+  const getShallowFilter = () => {
+    if (!shallowFilters) {
+      shallowFilters = shallow(<Filters {...props} />);
+    }
+    return shallowFilters;
+  }
+
+  beforeEach(() => {
+    shallowFilters = undefined;
+  });
+
+  test('Component should render without crashing', () => {
+    expect(getShallowFilter()).toBeDefined();
+  });
+
+  test('Component should render correctly', () => {
+    expect(getShallowFilter()).toMatchSnapshot();
+  });
+
+  test('Component should call filterCallback on click over submit', () => {
+    const filter = mount(<Filters {...props} />);
+    const filterForm = filter.find('Form');
+    filterForm.simulate('submit', { preventDefault: jest.fn(), key: 'Enter' });
+    expect(props.filterCallback).toHaveBeenCalled();
+  });
+});
