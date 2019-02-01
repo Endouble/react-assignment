61b9b3f0cf422c21eec50df0bb9a5c554853b234
hmurrar <thug4life>
2019-02-01 16:17:04 +0200

    fix giphy service

--- src/services/giphy.js
+++ src/services/giphy.js
@@ -1,15 +1,17 @@
-import serializeObject from '../helpers/serializerr';
+import { serialize } from '../helpers';
 
 // search giphy with pagination
-const searchGiphy = (options = {}) => {
-    const params = serializeObject(Object.assign(options, { api_key: process.env.REACT_APP_GIPHY_API_KEY }));
+const giphyService = (options = {}) => {
+    const params = serialize(Object.assign(options, { api_key: process.env.REACT_APP_GIPHY_API_KEY }));
     return fetch(`/search?${params}`)
         .then(resp => resp.json())
         .then(data => data)
         .catch((err) => {
-            // eslint-disable-next-line no-use-before-define
-            console.error(err);
+            /* eslint-disable no-console */
+            console.error(err.message || err);
+            /* eslint-enable no-console */
+            throw Error(err);
         });
 };
 
-export default searchGiphy;
+export default giphyService;
