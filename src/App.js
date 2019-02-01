ecbef1c5287fadf022ce081413e3760939339b42
hmurrar <thug4life>
2019-02-01 16:22:33 +0200

    import FilterForm | fix App component | fix App.test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+// components
 import Grid from './components/Grid';
-import './App.scss';
+import FilterForm from './components/FilterForm';
 
-const App = () => (
-    <div className="App">
-        <Grid />
-    </div>
-);
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchString: '',
+        };
+
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    handleSearch(string) {
+        this.setState({
+            searchString: string,
+        });
+    }
+
+    render() {
+        const { searchString } = this.state;
+        return (
+            <div className="App">
+                <FilterForm handleSearch={this.handleSearch} />
+                <Grid searchText={searchString} />
+            </div>
+        );
+    }
+}
 
 export default App;
